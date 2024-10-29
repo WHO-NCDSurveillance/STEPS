0beabedab210fa45c4b3eb0272a70bd25797dcc1
lucasmalla <lucasmatking@gmail.com>
2024-10-29 14:38:11 +0300

    FOlder with STEPS final scripts

---
+++ STEPS final scripts/Report templates by Stefan/version 6_SS/scripts/functions/table_formatting_function_demog.R
@@ -0,0 +1,91 @@
+#column_strat = c('all','collevel1_2','collevel2','total_col', 'total_overall','total_row')
+extracted_integers = unique(as.integer(unlist(str_extract_all(column_strat, "\\d+"))))
+collevel = paste0('collevel',paste0(extracted_integers, collapse = '_'))
+if('sex' %in% col_strat_variable){col_strat_var_levels = names(table(data[,'demog_c1']))}
+
+col_strat_var_levels[col_strat_var_levels==other_language[1,2]]='Men'
+col_strat_var_levels[col_strat_var_levels==other_language[2,2]]='Women'
+
+
+if(column_strat =='all')
+  {
+edited_table = pre_edited_table %>% flextable() %>% autofit() %>% delete_part(part = "header") %>%
+  flextable::style(pr_t=fp_text(font.family='Source Sans Pro'), part = 'all')%>%
+  bold(i = c(1:2,total_pos+3))%>% 
+  fontsize(size = 8.5 ,part = "all") %>% border_remove() %>%
+  theme_vanilla() %>% 
+  merge_h(i = 1:2) %>%
+  padding(padding = 0, part = "all")%>%paginate(init = TRUE, hdr_ftr = TRUE)%>%
+  align(align = "center", j=1:7, part = "body")  %>% vline(j=vline_num, border = std_border)%>%
+  hline(i=final_hlines, border = white_border)
+}else if(column_strat == collevel)
+{
+  incl_level_names = col_strat_var_levels[extracted_integers]
+  pre_edited_table = pre_edited_table[,c(1,grep(paste0(incl_level_names, collapse = '|'),names(pre_edited_table)))]
+  #############
+  if(length(row_strat_variables)>1)
+  {
+    all_additional_levels = eval(parse(text = paste0('sum(c(',paste0('length(names(table(analysis_data$',row_strat_variables[-1],')))', collapse = ','),'))')))
+    pre_edited_table = pre_edited_table[c(1:3,tail(1:nrow(pre_edited_table),all_additional_levels+2)),]
+  }else{
+    pre_edited_table = pre_edited_table[c(1:3,nrow(pre_edited_table)),]
+  }
+  #############
+  edited_table = pre_edited_table %>% flextable() %>% autofit() %>% delete_part(part = "header") %>%
+    flextable::style(pr_t=fp_text(font.family='Source Sans Pro'), part = 'all')%>%
+    bold(i = c(1:2,total_pos+3))%>% 
+    fontsize(size = 8.5 ,part = "all") %>% border_remove() %>%
+    theme_vanilla() %>% 
+    merge_h(i = 1:2) %>%
+    padding(padding = 0, part = "all")%>%paginate(init = TRUE, hdr_ftr = TRUE)%>%
+    align(align = "center", j=1:ncol(pre_edited_table), part = "body")%>%
+    vline(j=3, border = std_border)#%>%
+    #hline(i=final_hlines, border = white_border)
+  }else if(column_strat == 'total_col')
+{
+    pre_edited_table = pre_edited_table[,-c(grep(paste0(c(col_strat_var_levels,'Men','Women'), collapse = '|'),names(pre_edited_table)))]
+    
+    edited_table = pre_edited_table%>%flextable() %>% autofit() %>% delete_part(part = "header") %>%
+      flextable::style(pr_t=fp_text(font.family='Source Sans Pro'), part = 'all')%>%
+      bold(i = c(1:2,total_pos+3))%>% 
+      fontsize(size = 8.5 ,part = "all") %>% border_remove() %>%
+      theme_vanilla() %>% 
+      merge_h(i = 1:2) %>%
+      padding(padding = 0, part = "all")%>%paginate(init = TRUE, hdr_ftr = TRUE)%>%
+      align(align = "center", j=1:ncol(pre_edited_table), part = "body")  %>% 
+      hline(i=final_hlines, border = white_border)
+    
+  }else if(column_strat == 'total_row')
+{
+    pre_edited_table = pre_edited_table[c(1:3,nrow(pre_edited_table)),]
+    
+    edited_table = pre_edited_table%>%flextable() %>% autofit() %>% delete_part(part = "header") %>%
+      flextable::style(pr_t=fp_text(font.family='Source Sans Pro'), part = 'all')%>%
+      bold(i = c(1:2,4))%>% 
+      fontsize(size = 8.5 ,part = "all") %>% border_remove() %>%
+      theme_vanilla() %>% 
+      merge_h(i = 1:2) %>%
+      padding(padding = 0, part = "all")%>%paginate(init = TRUE, hdr_ftr = TRUE)%>%
+      align(align = "center", j=1:ncol(pre_edited_table), part = "body") 
+    
+  } else if (column_strat == 'total_overall')
+  {
+    if(length(row_strat_variables)>1)
+    {
+      all_additional_levels = eval(parse(text = paste0('sum(c(',paste0('length(names(table(analysis_data$',row_strat_variables[-1],')))', collapse = ','),'))')))
+      pre_edited_table = pre_edited_table[c(1:3,tail(1:nrow(pre_edited_table),all_additional_levels+2)),-c(grep(paste0(c(col_strat_var_levels,'Men','Women'), collapse = '|'),names(pre_edited_table)))]
+    }else{
+      pre_edited_table = pre_edited_table[c(1:3,nrow(pre_edited_table)),-c(grep(paste0(c(col_strat_var_levels,'Men','Women'), collapse = '|'),names(pre_edited_table)))]
+    }
+    
+    edited_table = pre_edited_table%>%flextable() %>% autofit() %>% delete_part(part = "header") %>%
+      flextable::style(pr_t=fp_text(font.family='Source Sans Pro'), part = 'all')%>%
+      bold(i = c(1:2,4))%>% 
+      fontsize(size = 8.5 ,part = "all") %>% border_remove() %>%
+      theme_vanilla() %>% 
+      merge_h(i = 1:2) %>%
+      padding(padding = 0, part = "all")%>%paginate(init = TRUE, hdr_ftr = TRUE)%>%
+      align(align = "center", j=1:ncol(pre_edited_table), part = "body")  
+    
+  }else{}
+
---
+++ STEPS final scripts/scripts/0_main_script.R
@@ -0,0 +1,67 @@
+# Remove all objects from the current R environment
+rm(list = ls())
+
+# Set the parent directory as the working directory
+#setwd("~/Documents/Transferred files/Data/data part 4/WHO/Task order 2/Scripts v5")
+# Get the source directory of the current executing script
+srcdir <- getSrcDirectory(function(){})[1]
+
+# Check if srcdir is empty, then use the current active document's directory
+if (srcdir == "") {
+  srcdir <- dirname(rstudioapi::getActiveDocumentContext()$path)
+}
+
+# Move one level up from the current srcdir
+parent_dir <- dirname(srcdir)
+# Set the working directory to the parent directory
+setwd(parent_dir)
+rm(srcdir, parent_dir)
+#setwd(normalizePath(file.path(getwd(), "..")))
+# Set survey options to handle 'lonely' primary sampling units (PSUs)
+# 'certainty' ensures lonely PSUs are treated with certainty weights
+options(survey.lonely.psu="certainty")
+# Adjust for lonely PSUs in domain analyses
+options(survey.adjust.domain.lonely=TRUE)
+
+### Loading required packages
+# List of packages needed for the analysis
+list_packages = c('openxlsx','readxl','tidyverse','survey','stringr',
+                  'officer','flextable','readstata13','dplyr','haven','polyglotr','magrittr') #,'officerWinTools'
+##devtools::install_github("joshmire/officerWinTools")
+# Load each package in the list
+eval(parse(text = paste0('library(',list_packages,')', sep='\n')))
+
+######################## Specifying analysis inputs ########################################
+
+### Define the country and year of the survey
+country = 'Lebanon'      # Country for the survey
+survey_year = 2024       # Year of the survey
+
+### Define stratification variables for the analysis
+col_strat_variable = c('sex')              # Column stratifier (e.g., sex)
+row_strat_variables = c('agerange')  # Row stratifiers (e.g., age range, nationality)
+row_strat_variable_titles =c("Age Categories (Years)")  # Titles for row stratifiers
+vars_exempt_77_88 = c('')                  # Variables exempt from specific conditions (e.g., missing codes)
+
+### Denominator limit: Minimum sample size required for estimating point estimates and confidence intervals
+denom_limit = 1
+
+### Language setting for the analysis
+language =c('ENGLISH','FRENCH','ARABIC', 'SPANISH','RUSSIAN','OTHER')[1]  # Select language
+if (language =='OTHER'){language = 'SPECIFY'} else{}  # Handle 'OTHER' language option
+language = tolower(language)  # Convert language to lowercase for consistency
+
+### Inputs for cardiovascular disease (CVD) risk computation
+country_ISO = 'LBN'  # ISO code for the country
+
+### Check if the specified ISO code exists in the reference dataset
+ISO_existence = country_ISO %in% unique(read_dta('scripts/functions/risk_ref_data.dta')$ccode)
+# If the ISO code does not exist, stop the script and display an error message
+if(!ISO_existence)stop('The supplied ISO code does not exist in the reference dataset for cvd risk calculation')
+
+################# Running scripts #################
+
+# Source external R scripts to process data and generate outputs
+source('scripts/1_data_processing_script.R')  # Data processing script
+source('scripts/2_databook_generation.R')     # Databook generation script
+source('scripts/3_factsheet_generation.R')    # Factsheet generation script
