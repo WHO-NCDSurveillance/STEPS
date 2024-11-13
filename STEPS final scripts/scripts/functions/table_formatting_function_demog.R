9d1abd1690c29fe4be8957b8109c1aaf8d699c4b
lucasmalla <49912636+lucasmalla@users.noreply.github.com>
2024-11-13 11:53:40 +0300

    Updated table_formatting_function_demog.R

--- STEPS final scripts/scripts/functions/table_formatting_function_demog.R
+++ STEPS final scripts/scripts/functions/table_formatting_function_demog.R
@@ -6,6 +6,8 @@ extracted_integers = unique(as.integer(unlist(str_extract_all(column_strat, "\\d
 collevel = paste0('collevel',paste0(extracted_integers, collapse = '_'))
 # Check if 'sex' is part of the column stratification variables and extract the levels accordingly.
 if('sex' %in% col_strat_variable){col_strat_var_levels = names(table(data[,'demog_c1']))}
+##
+full_tab = ifelse(length(grep('full',column_strat, v=T))==1,TRUE, FALSE))
 # Replace specific level names in the column stratification variables with 'Men' and 'Women'
 # based on the language settings.
 col_strat_var_levels[col_strat_var_levels==other_language[1,2]]='Men'
@@ -24,7 +26,7 @@ edited_table = pre_edited_table %>% flextable() %>% autofit() %>% delete_part(pa
   align(align = "center", j=1:7, part = "body")  %>% vline(j=vline_num, border = std_border)%>%
   hline(i=final_hlines, border = white_border)
 # If column_strat matches collevel, apply specific stratification based on the extracted levels.
-}else if(column_strat == collevel)
+}else if(column_strat == collevel & full_tab == FALSE)
 {
   incl_level_names = col_strat_var_levels[extracted_integers]
   # Filter columns based on the included level names.
@@ -50,6 +52,22 @@ edited_table = pre_edited_table %>% flextable() %>% autofit() %>% delete_part(pa
     vline(j=3, border = std_border)#%>%
     #hline(i=final_hlines, border = white_border)
   # Handle case where column_strat is 'total_col', which removes certain columns from the table.
+  }else if(column_strat == collevel & full_tab == TRUE)
+{
+    incl_level_names = col_strat_var_levels[extracted_integers]
+    # Filter columns based on the included level names.
+    pre_edited_table = pre_edited_table[,c(1,grep(paste0(incl_level_names, collapse = '|'),names(pre_edited_table)))]
+    # Format the table with the specified options.
+    edited_table = pre_edited_table%>%flextable() %>% autofit() %>% delete_part(part = "header") %>%
+      flextable::style(pr_t=fp_text(font.family='Source Sans Pro'), part = 'all')%>%
+      bold(i = c(1:2,total_pos+3))%>% 
+      fontsize(size = 8.5 ,part = "all") %>% border_remove() %>%
+      theme_vanilla() %>% 
+      merge_h(i = 1:2) %>%
+      padding(padding = 0, part = "all")%>%paginate(init = TRUE, hdr_ftr = TRUE)%>%
+      align(align = "center", j=1:ncol(pre_edited_table), part = "body")  %>% 
+      hline(i=final_hlines, border = white_border)
+    # Handle case where column_strat is 'total_row', which limits the table to certain rows.
   }else if(column_strat == 'total_col')
 {
     pre_edited_table = pre_edited_table[,-c(grep(paste0(c(col_strat_var_levels,'Men','Women'), collapse = '|'),names(pre_edited_table)))]
@@ -97,4 +115,3 @@ edited_table = pre_edited_table %>% flextable() %>% autofit() %>% delete_part(pa
       align(align = "center", j=1:ncol(pre_edited_table), part = "body")  
     
   }else{}
-
