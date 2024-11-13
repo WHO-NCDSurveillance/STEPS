15ae5e8df6bf0409d4f476d066da1e36262e2b92
lucasmalla <49912636+lucasmalla@users.noreply.github.com>
2024-11-13 11:55:41 +0300

    Update table_formatting_function_nondemog.R

--- STEPS final scripts/scripts/functions/table_formatting_function_nondemog.R
+++ STEPS final scripts/scripts/functions/table_formatting_function_nondemog.R
@@ -1,3 +1,4 @@
+#trial_table = pre_sub_edited_table
 #column_strat = c('all','collevel1_2','collevel2','total_col', 'total_overall','total_row')
 # Extract unique integers from the 'column_strat' string (e.g., 'collevel1_2') using a regex pattern.
 # These integers will be used to determine specific levels for stratification.
@@ -5,6 +6,8 @@ extracted_integers = unique(as.integer(unlist(str_extract_all(column_strat, "\\d
 # Create a string 'collevel' that represents a specific column stratification level based on the extracted integers.
 # For example, if 'column_strat' contains "collevel1_2", 'collevel' will be set to "collevel1_2".
 collevel = paste0('collevel',paste0(extracted_integers, collapse = '_'))
+##
+full_tab = ifelse(length(grep('full',column_strat, v=T))==1,TRUE, FALSE)
 # If 'sex' is among the stratification variables, retrieve the levels for the 'demog_c1' variable from the data.
 if('sex' %in% col_strat_variable){col_strat_var_levels = names(table(data[,'demog_c1']))}
 # Map specific language strings in 'col_strat_var_levels' to 'Men' and 'Women'.
@@ -25,7 +28,7 @@ if(column_strat =='all')
     align(align = "center", j=1:ncol(pre_sub_edited_table), part = "body")  %>% vline(j=vline_num, border = std_border)%>%
     hline(i=final_hlines, border = white_border)
   # Case 2: If 'column_strat' matches 'collevel', filter and format specific columns based on the extracted level names.
-}else if(column_strat == collevel)
+}else if(column_strat == collevel & full_tab == FALSE)
 {
   # Extract the stratification levels based on the integers obtained earlier.
   incl_level_names = col_strat_var_levels[extracted_integers]
@@ -52,6 +55,23 @@ if(column_strat =='all')
     align(align = "center", j=1:ncol(pre_sub_edited_table), part = "body")#%>%
     #hline(i=final_hlines, border = white_border)
   # Case 3: If 'column_strat' is set to 'total_col', exclude specific stratification columns and format the table.
+}else if(column_strat == collevel & full_tab == TRUE)
+{
+  # Extract the stratification levels based on the integers obtained earlier.
+  incl_level_names = col_strat_var_levels[extracted_integers]
+  # Subset the table to only include relevant columns based on the stratification levels.
+  pre_sub_edited_table = pre_sub_edited_table[,c(1,grep(paste0(incl_level_names, collapse = '|'),names(pre_sub_edited_table)))]
+  # Apply table styling for the 'total_col' case.
+  sub_edited_table = pre_sub_edited_table%>%flextable() %>% autofit() %>% delete_part(part = "header") %>%
+    flextable::style(pr_t=fp_text(font.family='Source Sans Pro'), part = 'all')%>%
+    bold(i = c(1:2,total_pos+3))%>% 
+    fontsize(size = 8.5 ,part = "all") %>% border_remove() %>%
+    theme_vanilla() %>% 
+    merge_h(i = 1:2) %>%
+    padding(padding = 0, part = "all")%>%paginate(init = TRUE, hdr_ftr = TRUE)%>%
+    align(align = "center", j=1:ncol(pre_sub_edited_table), part = "body")  %>% 
+    hline(i=final_hlines, border = white_border)
+  # Case 4: If 'column_strat' is set to 'total_row', subset rows to include only relevant summary information.
 }else if(column_strat == 'total_col')
 {
   # Remove columns that match any of the specified stratification levels.
