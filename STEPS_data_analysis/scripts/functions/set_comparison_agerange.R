0e125350d30b12ef0306463c243bfbd77ca61fbf
lucasmalla <49912636+lucasmalla@users.noreply.github.com>
2026-02-11 06:08:10 +0000

    Update set_comparison_agerange.R
    Fixed the upper cut-offs in the line:     agerange = case_when(age>=18 & age <30 ~1,age>=30 & age <45 ~2,
                             age>=45 & age <60 ~3,age>=60 & age <69 ~4)

--- STEPS_data_analysis/scripts/functions/set_comparison_agerange.R
+++ STEPS_data_analysis/scripts/functions/set_comparison_agerange.R
@@ -2,8 +2,8 @@
 combined_dataset = combined_dataset %>%
   mutate(
     ##Overwriting agerange variable
-    agerange = case_when(age>=18 & age <29 ~1,age>=30 & age <44 ~2,
-                         age>=45 & age <59 ~3,age>=60 & age <69 ~4),
+    agerange = case_when(age>=18 & age <30 ~1,age>=30 & age <45 ~2,
+                         age>=45 & age <60 ~3,age>=60 & age <69 ~4),
     agerange = factor(agerange,levels=1:4, labels=c('18-29','30-44','45-59','60-69')),
     ######
     sex_age = case_when(sex == 'Men' & (agerange=='18-29'|agerange=='30-44') ~ 1,
@@ -15,4 +15,5 @@ combined_dataset = combined_dataset %>%
     bin_age = case_when(agerange=='18-29'|agerange=='30-44' ~ 1,
                         agerange=='45-59'|agerange=='60-69' ~ 2),
     bin_age = factor(bin_age,levels = 1:2, labels = c('18-44','45-69')))%>%
+
   dplyr::filter(!is.na(agerange))
